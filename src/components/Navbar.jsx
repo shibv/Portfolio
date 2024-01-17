1306c0e303346baa09e572bd11e3026d1fe6dc04
shiv <ps431352@gmail.com>
2024-01-17 22:43:27 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             <li className="p-4"><a href="#skills" className="hover:underline">Skills</a></li>
             <li className="p-4"><a href="#certs" className="hover:underline">Projects</a></li>
             </ul>
-            <a href={resume} className='hover:scale-105 transition-all duration-500 cursor-pointer' >
+            <a href="#" className='hover:scale-105 transition-all duration-500 cursor-pointer' >
             <button className='inline-flex h-12  items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
         Resume
       </button>
