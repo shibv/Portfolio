de40dd793bb6756f72a8be24c31049deba4dd26a
shiv-shopflo <shiv.singh@shopflo.com>
2025-01-25 22:41:09 +0530

    changed icon img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import cov from '../assets/cov-3.png'
+import cov from '../assets/img.png'
 
 
 
@@ -16,7 +16,7 @@ const Navbar = () => {
             <button className='inline-flex h-12  items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
         Resume
       </button>
-            </a> */}
+            </a> */}  
         </div>
     </div>
   )
